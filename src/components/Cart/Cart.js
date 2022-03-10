96fe979d397fa1a38baee4fdee00f9af50144688
MercEzz <miteshkvirash@gmail.com>
2022-03-11 02:27:49 +0530

    Modal added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,74 @@
+import {
+  Flex,
+  ListItem,
+  UnorderedList,
+  Text,
+  Box,
+  Button,
+} from "@chakra-ui/react";
+import Modal from "../UI/Modal";
+
+const Cart = (props) => {
+  const carItems = (
+    <UnorderedList listStyleType="none" margin="0" p="0">
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <ListItem>{item.name}</ListItem>
+      ))}
+    </UnorderedList>
+  );
+
+  return (
+    <>
+      <Modal>
+        {carItems}
+        <Flex
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          fontWeight="bold"
+          fontSize="1.5rem"
+          m="1rem 0"
+        >
+          <Text>Total Amount</Text>
+          <Text>35.62</Text>
+        </Flex>
+        <Box textAlign="right">
+          <Button
+            fontStyle="inherit"
+            cursor="pointer"
+            bgColor="transparent"
+            color="grey.500"
+            border="1px solid grey.500"
+            p="0.5rem 2rem"
+            borderRadius="25px"
+            ml="1rem"
+            _hover={{
+              bgColor: "grey",
+              borderColor: "grey",
+              color: "white",
+            }}
+            _active={{ bgColor: "grey", borderColor: "grey", color: "white" }}
+          >
+            Close
+          </Button>
+          <Button
+            fontStyle="inherit"
+            cursor="pointer"
+            bgColor="grey"
+            color="white"
+            border="1px solid grey.500"
+            p="0.5rem 2rem"
+            borderRadius="25px"
+            ml="1rem"
+            _hover={{ bgColor: "white", borderColor: "grey", color: "grey" }}
+            _active={{ bgColor: "grey", borderColor: "grey", color: "white" }}
+          >
+            Order
+          </Button>
+        </Box>
+      </Modal>
+    </>
+  );
+};
+
+export default Cart;
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,50 @@
+import { Box } from "@chakra-ui/react";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return (
+    <Box
+      pos="fixed"
+      top="0"
+      left="0"
+      w="100%"
+      h="100vh"
+      zIndex="20"
+      bgColor="rgba(0,0,0, 0.75)"
+    />
+  );
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Box
+      pos="fixed"
+      top="20vh"
+      left={{ base: "5%", lg: "calc(50% - 20rem)" }}
+      w={{ base: "90%", lg: "40rem" }}
+      bgColor="white"
+      p="1rem"
+      borderRadius="14px"
+      boxShadow="0 2px 8px rgba(0,0,0, 0.25)"
+      zIndex="30"
+    >
+      {props.children}
+    </Box>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
